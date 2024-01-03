d6c012de60c07608a3eabb5d529a623cead7901c
beststarr <119686833+beststarr@users.noreply.github.com>
2024-01-03 08:12:12 +0700

    Update var_global.dart

--- lib/utils/var_global.dart
+++ lib/utils/var_global.dart
@@ -6,14 +6,28 @@ class VarGlobal {
           '1. Berikan Pelukan pada Anak. Cara mengatasi tantrum pada anak yang pertama adalah dengan memberikan pelukan.\n2. Tetap tenang dan temani anak saat tantrum. ...\n3. Alihkan Perhatian Anak. ...\n4. Hindari Hukuman Fisik. ...\n5. Cari Tahu Penyebabnya. ...'
     },
     {
-      'title': 'Cara mengatasi anak tantrum',
+      'title': 'Gizi Buruk Anak',
       'deskripsi':
-          '1. Berikan Pelukan pada Anak. Cara mengatasi tantrum pada anak yang pertama adalah dengan memberikan pelukan.\n2. Tetap tenang dan temani anak saat tantrum. ...\n3. Alihkan Perhatian Anak. ...\n4. Hindari Hukuman Fisik. ...\n5. Cari Tahu Penyebabnya. ...'
+          'Gizi buruk adalah kondisi ketika berat badan anak terlalu rendah bila dibandingkan dengan tinggi badannya. Anak dengan gizi buruk atau severe wasting biasanya memiliki daya tahan tubuh yang sangat lemah sehingga berisiko terkena penyakit parah, bahkan meninggal.'
     },
     {
-      'title': 'Cara mengatasi anak tantrum',
+      'title': 'Penyebab Anak Susah Makan dan Cara Mengatasinya',
       'deskripsi':
-          '1. Berikan Pelukan pada Anak. Cara mengatasi tantrum pada anak yang pertama adalah dengan memberikan pelukan.\n2. Tetap tenang dan temani anak saat tantrum. ...\n3. Alihkan Perhatian Anak. ...\n4. Hindari Hukuman Fisik. ...\n5. Cari Tahu Penyebabnya. ...'
+          'Menghadapi anak susah makan memang diperlukan kesabaran dan strategi tersendiri. Oleh karena itu, penting bagi setiap orang tua untuk memahami penyebab anak susah makan dan cara mengatasinya sehingga kebutuhan nutrisi anak tetap terpenuhi. \n Anak-anak biasanya susah makan atau menjadi picky eater ketika usianya menginjak 1 tahun. Namun, ada juga anak yang susah makan ketika usianya 2–5 tahun.'
     },
+  ];
+}
+
+class IsiArtikel {
+  static List<Map> isi_artikel = [
+    {
+      'isi': "Tantrum adalah kondisi saat anak mengeluarkan emosinya dengan beragam cara. Tantrum pada anak biasanya terjadi pada anak yang masih berusia 1-4 tahun karena ketidakmampuan anak dalam menjelaskan apa yang menjadi keinginannya. Cara Mengatasi Tantrum pada Anak\n 1. Berikan Pelukan pada Anak\n Memberikan pelukan pada anak berguna untuk meredakan emosinya. \n 2. Temani Anak \n Menemani anak saat mereka tantrum berguna agar mereka tidak merasa diabaikan. 3. Alihkan Perhatian Anak \n Mengalihkan perhatian anak bisa dengan mengajak jalan-jalan atau memberikan makanan favoritnya. \n 4. Hindari Hukuman Fisik \n Memberikan hukuman fisik seperti memukul anak pada saat tantrum bukanlah hal yang baik. Jenis pola asuh seperti ini justru akan memberikan contoh buruk pada anak. \n 5. Cari Tahu Penyebabnya \n Jika tantrum pada anak sudah mereda, ajak anak berkomunikasi untuk tahu penyebabnya. \n Jika Anda telah melakukan sejumlah cara di atas, tapi tantrum anak tetap sering terjadi dan semakin parah, segera konsultasikan dengan dokter Spesialis Anak Siloam Hospitals untuk mendapatkan penanganan tepat sesuai jenis dan penyebab tantrum.",
+    },
+    {
+      'isi': "Gizi buruk adalah kondisi ketika berat badan anak terlalu rendah bila dibandingkan dengan tinggi badannya. Anak dengan gizi buruk atau severe wasting biasanya memiliki daya tahan tubuh yang sangat lemah sehingga berisiko terkena penyakit parah, bahkan meninggal. \n Penyebab utama gizi buruk adalah kekurangan asupan makanan yang bernutrisi sesuai kebutuhan masing-masing kelompok usia anak. Kekurangan asupan ini bisa terjadi karena tidak tersedianya bahan makanan yang berkualitas baik. \n Selain itu, gizi buruk juga sering disebabkan oleh gangguan penyerapan nutrisi akibat penyakit kronis, misalnya diare kronis atau TBC. \n Anak dengan gizi buruk perlu menjalani rawat inap di rumah sakit agar dokter dapat menstabilkan kondisi dan tanda-tanda vital anak. ",
+    },
+    {
+      'isi': "Menghadapi anak susah makan memang diperlukan kesabaran dan strategi tersendiri. Oleh karena itu, penting bagi setiap orang tua untuk memahami penyebab anak susah makan dan cara mengatasinya sehingga kebutuhan nutrisi anak tetap terpenuhi. \n Anak-anak biasanya susah makan atau menjadi picky eater ketika usianya menginjak 1 tahun. Namun, ada juga anak yang susah makan ketika usianya 2–5 tahun. \n Tips Menghadapi Anak Susah Makan \n Bagi anak-anak, makan termasuk dalam proses belajar dan eksplorasi. Untuk menambah selera makan pada anak yang susah makan, ada beberapa hal yang dapat dilakukan, yaitu: \n 1. Adakan acara makan keluarga secara rutin dan biarkan Si Kecil melihat orang sekitarnya mengonsumsi beragam jenis makanan sehat. \n 2. Buatlah jadwal makan yang tetap, yaitu 3 kali waktu makan utama dan 2 kali makan camilan setiap hari, serta batasi waktu sekitar 30 menit untuk setiap waktu makan. \n 3. Biarkan Si Kecil makan sendiri dan berikan makanan yang mudah dipegang dan dimasukkan ke dalam mulutnya. \n 4. Berikan makanan dalam porsi kecil terlebih dahulu dan puji Si Kecil ketika berhasil menghabiskannya. \n 5. Gunakan perlengkapan makan dengan gambar dan warna menarik atau yang disukai oleh Si Kecil.",
+    }
   ];
 }
